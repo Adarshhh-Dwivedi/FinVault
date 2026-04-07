decabcb72157eb849523892d38b041a85b500a32
Adarsh Dwivedi <adarshdwivedi619@gmail.com>
2026-04-07 22:40:13 +0530

    added swagger config to test the api

---
+++ SpringBank/src/main/java/com/example/SpringBank/configuration/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.example.SpringBank.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Updated for Java 21, Spring Boot 3.4.4, and SpringDoc OpenAPI
+ */
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI springBankOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("SpringBank API")
+                        .description("Modernized Bank API Definition")
+                        .version("v1.0.0")
+                        .contact(new Contact()
+                                .name("Karan Sharma") // Updated to your preferred name
+                                .url("https://github.com/your-profile")
+                                .email("karan@example.com"))
+                        .license(new License()
+                                .name("MIT")
+                                .url("http://springdoc.org")));
+    }
+}
